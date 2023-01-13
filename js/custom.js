5e04f0a72848f6bb5f43c9ed6fcbcc26f3353d1a
ismoilovDev <ismoilovabbosdev@gmail.com>
2023-01-13 23:58:20 +0500

    rrr

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+const msg_input = document.querySelector('#msg_input'),
+        msg_text = document.querySelector('#msg_text'),
+        send_msg = document.querySelector('#send_msg');
+
+
+
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+getYear();
+
+function sendMsg() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
