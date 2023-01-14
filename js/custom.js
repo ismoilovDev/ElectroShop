f87fbbad8575dd9bea32512f2b924c1d3bf206de
ismoilovDev <ismoilovabbosdev@gmail.com>
2023-01-14 23:49:06 +0500

    tel nomer

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,8 @@
-const msg_input = document.querySelector('#msg_input'),
-        msg_text = document.querySelector('#msg_text'),
-        send_msg = document.querySelector('#send_msg');
-
+const msg_input = document.querySelector('.msg_input'),
+        name_input = document.querySelector('.name_input'),
+        msg_text = document.querySelector('.msg_text'),
+        msg_send_form = document.querySelector('.msg_send_form'),
+        send_msg = document.querySelector('.send_msg');
 
 
 // to get current year
@@ -12,16 +13,26 @@ function getYear() {
 }
 getYear();
 
-function sendMsg() {
+function sendMsg(e) {
+    e.preventDefault()
+    let text = `
+        Assalomu alaykum! <br/> 
+        Mening Ismim ${name_input.value} <br />
+        A'loqa Uchun ${msg_input.value} <br/>
+        <h3>${msg_text.value} </h3>
+    `
+    "Assalomu alaykum! <br/> Mening Ismim " +  name_input.value + " " + "<br/> Xabarim " + msg_text.value
     Email.send({
         Host : "smtp.elasticemail.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
+        Username : "axmedovadilzoda@gmail.com",
+        Password : "F81321A2DB443E52EE5998DA0F1D0349BD29",
+        To : 'electroshopuzb@gmail.com',
+        From : "axmedovadilzoda@gmail.com",
         Subject : "This is the subject",
-        Body : "And this is the body"
+        Body : text
     }).then(
       message => alert(message)
     );
 }
+
+msg_send_form.addEventListener('submit', sendMsg)
